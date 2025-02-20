f596980d31f43ceb2505f7a530199b28cde1fef2
GuilhermeSilva <guilherme.silva672@fatec.sp.gov.br>
2025-02-20 17:34:14 -0300

    Exercício 1007 - INICIANTE

---
+++ Iniciante/1007/JavaScript/1007.js
@@ -0,0 +1,6 @@
+let A = parseInt(prompt());
+let B = parseInt(prompt());
+let C = parseInt(prompt());
+let D = parseInt(prompt());
+let DIFERENCA = A * B - C * D;
+console.log(`DIFERENCA = ${DIFERENCA}`);
