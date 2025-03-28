849e5bbf88801770a907d485617ca8bb8d7dd525
GuilhermeSilva <guilherme.silva672@fatec.sp.gov.br>
2025-03-27 23:30:29 -0300

    Refatoração de alguns códigos

--- Iniciante/1040/Python/1040.py
+++ Iniciante/1040/Python/1040.py
@@ -4,14 +4,14 @@ n2 = float(n2)
 n3 = float(n3)
 n4 = float(n4)
 
-media = (n1*2 + n2*3 + n3*4 + n4*1) / 10
+media = (n1*2 + n2*3 + n3*4 + n4) / 10
 print(f'Media: {media:.1f}')
 
 if media >= 7:
     print('Aluno aprovado.')
 elif media < 5:
     print('Aluno reprovado.')
-elif 5 <= media <= 6.9:
+else:
     print('Aluno em exame.')
     n5 = float(input())
     print(f'Nota do exame: {n5:.1f}')
