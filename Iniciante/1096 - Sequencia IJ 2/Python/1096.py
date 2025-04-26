d9eef249e96d410d0750fc5a2fc4433fc4f36ddb
GuilhermeSilva <guilherme.silva672@fatec.sp.gov.br>
2025-04-26 15:28:24 -0300

    Resolução 1095, 1096 e 1097 - INICIANTE

---
+++ Iniciante/1096 - Sequencia IJ 2/Python/1096.py
@@ -0,0 +1,15 @@
+def sequencia():
+    I = 1
+    J = 7
+    while I <= 9:
+        contador = 1
+        while contador <= 3:
+            print(f"I={I} J={J}")
+            J -= 1
+            contador += 1
+        I += 2
+        J = 7
+        contador = 1
+
+
+sequencia()
---
+++ Iniciante/1097 - Sequencia IJ 3/Python/1097.py
@@ -0,0 +1,16 @@
+def sequencia():
+    I = 1
+    J_inicial = 7
+    while I <= 9:
+        contador = 1
+        J = J_inicial
+        while contador <= 3:
+            print(f"I={I} J={J}")
+            J -= 1
+            contador += 1
+        I += 2
+        J_inicial += 2
+        contador = 1
+
+
+sequencia()
