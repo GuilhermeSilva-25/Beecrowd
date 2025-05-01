7f1bd4f3ab66c221261c654f85032b5bcd0cc78e
GuilhermeSilva <guilherme.silva672@fatec.sp.gov.br>
2025-05-01 20:00:42 -0300

    Resolução 1114 - INICIANTE

---
+++ Iniciante/1114 - Senha Fixa/Python/1114.py
@@ -0,0 +1,14 @@
+def senha():
+    senha_correta = 2002
+    validador = 0
+
+    while validador != 1:
+        senha = int(input())
+        if senha == senha_correta:
+            print("Acesso Permitido")
+            validador = 1
+        else:
+            print("Senha Invalida")
+
+
+senha()
