25e24c7cdd9e5705a112c8b0e7d598e60d8f1518
GuilhermeSilva <guilherme.silva672@fatec.sp.gov.br>
2025-05-02 20:18:56 -0300

    Resolução 1116 - INICIANTE e 1546 - AD-HOC

---
+++ Iniciante/1116 - Dividindo X por Y/Python/1116.py
@@ -0,0 +1,19 @@
+def divisao():
+    N = int(input())
+    contador = 0
+
+    while contador < N:
+        X, Y = input().split()
+        X = int(X)
+        Y = int(Y)
+
+        if Y == 0:
+            print("divisao impossivel")
+        else:
+            divisao = X / Y
+            print(f"{divisao:.1f}")
+
+        contador += 1
+
+
+divisao()
