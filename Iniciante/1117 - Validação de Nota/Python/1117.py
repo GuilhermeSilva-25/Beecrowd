38e4ac403922e0562b67b6e1e1a038af126642b1
GuilhermeSilva <guilherme.silva672@fatec.sp.gov.br>
2025-05-04 12:35:02 -0300

    Resolução 1117 - INICIANTE

---
+++ Iniciante/1117 - Validação de Nota/Python/1117.py
@@ -0,0 +1,18 @@
+def media():
+    verificador = 0
+    media = 0
+    contador = 0
+    while verificador == 0:
+        while contador < 2:
+            nota = float(input())
+
+            if 0 <= nota <= 10:
+                media += nota
+                contador += 1
+            else:
+                print("nota invalida")
+        media /= 2
+        print(f"media = {media:.2f}")
+        verificador = 1
+
+media()
