e234b8346d68d5a78dfd9e535ae23cd0a8ec4bf4
GuilhermeSilva <guilherme.silva672@fatec.sp.gov.br>
2025-05-05 18:01:05 -0300

    Resolução 1134 - INICIANTE

---
+++ Iniciante/1134 - Tipo de Combustível/Python/1134.py
@@ -0,0 +1,22 @@
+def combustivel():
+    verificador = 0
+    alcool = 0
+    gasolina = 0
+    diesel = 0
+
+    while verificador == 0:
+        produto = int(input())
+
+        if produto == 1:
+            alcool += 1
+        elif produto == 2:
+            gasolina += 1
+        elif produto == 3:
+            diesel += 1
+        elif produto == 4:
+            verificador = 1
+
+    print(f"MUITO OBRIGADO\nAlcool: {alcool}\nGasolina: {gasolina}\nDiesel: {diesel}")
+
+
+combustivel()
