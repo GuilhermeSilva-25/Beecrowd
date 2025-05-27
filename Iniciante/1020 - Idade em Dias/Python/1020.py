405bfc7de3a81c454d65d6ef0a89a8d9fe7bf9cc
GuilhermeSilva <guilherme.silva672@fatec.sp.gov.br>
2025-05-27 19:07:51 -0300

    Resolução Exercício 1020 - INICIANTE

--- Iniciante/1020 - Idade em Dias/Python/1020.py
+++ Iniciante/1020 - Idade em Dias/Python/1020.py
@@ -1,12 +1,17 @@
-def idade_em_dias():
+def main():
     idade_dias = int(input())
 
-    anos = idade_dias // 365
-    idade_dias -= anos * 365
-    mes = idade_dias // 30
-    idade_dias -= mes * 30
-    dia = idade_dias
-    print(f"{anos} ano(s)\n{mes} mes(es)\n{dia} dia(s)")
+    print(conversao(idade_dias))
 
 
-idade_em_dias()
+def conversao(x):
+    anos = x // 365
+    x -= anos * 365
+    mes = x // 30
+    x -= mes * 30
+    dia = x
+
+    return f"{anos} ano(s)\n{mes} mes(es)\n{dia} dia(s)"
+
+
+main()
