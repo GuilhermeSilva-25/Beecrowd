565a8987a9792dd5806d18ec100d02170d29b921
GuilhermeSilva <guilherme.silva672@fatec.sp.gov.br>
2025-05-27 18:46:56 -0300

    Resolução Exercício 1019 - INICIANTE

--- Iniciante/1019 - Conversão de Tempo/Python/1019.py
+++ Iniciante/1019 - Conversão de Tempo/Python/1019.py
@@ -1,16 +1,19 @@
-N = int(input())
+def main():
+    N = int(input())
+
+    print(conversao(N))
 
 
-def conversao():
+def conversao(n):
     horas = 0
     minutos = 0
     segundos = 0
 
-    horas = N // 3600
-    minutos = (N % 3600) // 60
-    segundos = (N % 3600) % 60
+    horas = n // 3600
+    minutos = (n % 3600) // 60
+    segundos = (n % 3600) % 60
 
     return f"{horas}:{minutos}:{segundos}"
 
 
-print(conversao())
+main()
