242143dba6780375b9ae3b4ad4b992743177d7b1
GuilhermeSilva <guilherme.silva672@fatec.sp.gov.br>
2025-05-28 13:58:36 -0300

    Resolução Exercício 1035 - INICIANTE

--- Iniciante/1035 -Teste de Seleção 1/Python/1035.py
+++ Iniciante/1035 -Teste de Seleção 1/Python/1035.py
@@ -1,14 +1,18 @@
-def teste():
+def main():
     A, B, C, D = input().split()
     A = int(A)
     B = int(B)
     C = int(C)
     D = int(D)
 
-    if B > C and D > A and (C + D) > (A + B) and C > 0 and D > 0 and A % 2 == 0:
-        print("Valores aceitos")
+    print(validacao(A, B, C, D))
+
+
+def validacao(a, b, c, d):
+    if b > c and d > a and (c + d) > (a + b) and c > 0 and d > 0 and a % 2 == 0:
+        return "Valores aceitos"
     else:
-        print("Valores nao aceitos")
+        return "Valores nao aceitos"
 
 
-teste()
+main()
