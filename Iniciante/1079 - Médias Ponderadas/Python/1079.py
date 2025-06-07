437cc5d7ce1e8a8ad40010a9e4259e65b89a4a16
GuilhermeSilva <guilherme.silva672@fatec.sp.gov.br>
2025-06-07 11:45:43 -0300

    Resolução Exercício 1079 - INICIANTE

--- Iniciante/1079 - Médias Ponderadas/Python/1079.py
+++ Iniciante/1079 - Médias Ponderadas/Python/1079.py
@@ -1,15 +1,21 @@
-def media_ponderada():
+def main():
     N = int(input())
-    contador = 0
 
-    while contador < N:
+    media_ponderada(N)
+
+
+def media_ponderada(n):
+    i = 0
+
+    while i < n:
         v1, v2, v3 = input().split()
         v1 = float(v1)
         v2 = float(v2)
         v3 = float(v3)
-        media = (v1 * 2 + v2 * 3 + v3 * 5) / (2 + 3 + 5)
+
+        media = (v1 * 2 + v2 * 3 + v3 * 5) / 10
         print(f"{media:.1f}")
-        contador += 1
+        i += 1
 
 
-media_ponderada()
+main()
