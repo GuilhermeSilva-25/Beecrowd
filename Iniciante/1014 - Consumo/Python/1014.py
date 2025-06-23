8d1d939dc0bedc05f5f5ef5f8fd8e641bd908fb6
GuilhermeSilva <guilherme.silva672@fatec.sp.gov.br>
2025-06-23 16:52:00 -0300

    Resolução 1014 - INICIANTE

--- Iniciante/1014 - Consumo/Python/1014.py
+++ Iniciante/1014 - Consumo/Python/1014.py
@@ -1,12 +1,8 @@
 def main():
     X = int(input())
     Y = float(input())
+    average_consumption = X / Y
+    print(f"{average_consumption:.3f} km/l")
 
-    print(f"{consumo(X, Y):.3f} km/l")
-
-
-def consumo(a, b):
-    return a / b
-
-
-main()
+if __name__ == "__main__":
+    main()
